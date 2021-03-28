efd3a0e0a459fa12cb4224961ba10a7178c9437d
Adi Muraru <amuraru@adobe.com>
2021-03-29 00:44:53 +0300

    [INTERNAL] - Use azul-openjdk jvm 16 in cruise-control docker image

--- docker/opt/cruise-control/start.sh
+++ docker/opt/cruise-control/start.sh
@@ -6,4 +6,6 @@ cd /opt/cruise-control
 if [ -z "$KAFKA_HEAP_OPTS" ]; then
   export KAFKA_HEAP_OPTS="-XX:InitialRAMPercentage=30 -XX:MaxRAMPercentage=85"
 fi
+# disable JEP 396: Strongly Encapsulate JDK Internals by Default (JDK-8256299)
+export KAFKA_HEAP_OPTS="--illegal-access=permit ${KAFKA_HEAP_OPTS}"
 /bin/bash ${DEBUG:+-x} /opt/cruise-control/kafka-cruise-control-start.sh /opt/cruise-control/config/cruisecontrol.properties 8090
